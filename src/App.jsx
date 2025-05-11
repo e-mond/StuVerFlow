1e030e074354e08a1049f1497d16059e7fdf1c53
e-mond <society.e@yahoo.com>
2025-05-11 10:18:52 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import AskQuestionPage from "./pages/AskQuestionPage";
+import QuestionDetailPage from "./pages/QuestionDetailPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/ask" element={<AskQuestionPage />} />
+        <Route path="/question/:id" element={<QuestionDetailPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
